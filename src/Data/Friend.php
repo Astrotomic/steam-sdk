762a754a4a42a261e3353ebb0e88b96aab6a7510
Tom Witkowski <dev.gummibeer@gmail.com>
2022-11-07 21:27:32 +0100

    improve friend DTO

--- src/Data/Friend.php
+++ src/Data/Friend.php
@@ -4,6 +4,7 @@ namespace Astrotomic\SteamSdk\Data;
 
 use Astrotomic\SteamSdk\Enums\Relationship;
 use Carbon\CarbonImmutable;
+use SteamID;
 
 final class Friend extends DataTransferObject
 {
@@ -12,5 +13,10 @@ final class Friend extends DataTransferObject
         public readonly Relationship $relationship,
         public readonly CarbonImmutable $friend_since,
     ) {
+    }
+
+    public function steamid(): SteamID
+    {
+        return new SteamID($this->steamid);
     }
 }
--- tests/Feature/Requests/GetFriendListRequestTest.php
+++ tests/Feature/Requests/GetFriendListRequestTest.php
@@ -8,4 +8,8 @@ it('returns friend list', function (string $steamid): void {
     $friends = app(SteamConnector::class)->getFriendList(steamid: $steamid);
 
     Assert::assertContainsOnlyInstancesOf(Friend::class, $friends);
+
+    $friends->each(function (Friend $friend): void {
+        Assert::assertSame($friend->steamid, $friend->steamid()->ConvertToUInt64());
+    });
 })->with('userids');
